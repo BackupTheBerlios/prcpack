40241385b08149787b896aceb35755a14bc46f1a
darkgod <darkgod>
2004-01-13 00:14:39 +0000

    howto use

--- prc_consortium/scripts/build.sh
+++ prc_consortium/scripts/build.sh
@@ -3,16 +3,16 @@
 twodas=`ls ../2das/*.2da | tr '\n' ' '`
 gfxs=`ls ../gfx/* | tr '\n' ' '`
 others=`ls ../others/* | tr '\n' ' '`
-tmp_scripts=`ls *.nss | tr '\n' ' '`
+scripts=`ls *.nss | tr '\n' ' '`
 
 # Take out the include files
-scripts=""
-for i in $tmp_scripts ; do
-	tst=`grep -w main "$i"`
-	if test "$tst" ; then
-		scripts="$scripts$i "
-	fi
-done
+#scripts=""
+#for i in $tmp_scripts ; do
+#	tst=`grep -w main "$i"`
+#	if test "$tst" ; then
+#		scripts="$scripts$i "
+#	fi
+#done
 
 sed "s#~~~scripts~~~#$scripts#g" Makefile | sed "s#~~~2das~~~#$twodas#g" | sed "s#~~~others~~~#$others#g" | sed "s#~~~gfxs~~~#$gfxs#g" > Makefile.temp
 make -f Makefile.temp $*
